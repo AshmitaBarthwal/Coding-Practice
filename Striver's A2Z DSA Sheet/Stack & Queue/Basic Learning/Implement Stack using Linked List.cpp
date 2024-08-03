1027f33a176063d24dc1209b68fc39e5a00e5e36
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-03 20:40:09 +0530

    Create Implement Stack using Linked List.cpp

---
+++ Striver's A2Z DSA Sheet/Stack & Queue/Basic Learning/Implement Stack using Linked List.cpp
@@ -0,0 +1,42 @@
+// https://www.geeksforgeeks.org/problems/implement-stack-using-linked-list/1
+
+
+//Brute: To push insert at the beginning of the linked list
+//To pop, pop from the beginning
+//Time: O(1)
+//Space: O(n)
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StackNode {
+    int data;
+    StackNode *next;
+
+    StackNode(int a) {
+        data = a;
+        next = NULL;
+    }
+};
+
+class MyStack {
+  private:
+    StackNode *top;
+
+  public:
+    void push(int x) {
+       StackNode* newnode = new StackNode(x);
+       if(top) newnode->next = top;
+       top = newnode;
+    }
+
+    int pop() {
+        if(!top) return -1;
+        int num = top->data;
+        StackNode* temp = top;
+        top = top->next;
+        delete (temp);
+        return num;
+    }
+
+    MyStack() { top = NULL; }
+};
