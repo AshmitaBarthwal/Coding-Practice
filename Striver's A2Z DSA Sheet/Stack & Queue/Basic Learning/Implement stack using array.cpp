24c9c74bc77097d7a98a1b120969866620ea9f6f
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-15 12:47:20 +0530

    Update Implement stack using array.cpp

--- Striver's A2Z DSA Sheet/Stack & Queue/Basic Learning/Implement stack using array.cpp
+++ Striver's A2Z DSA Sheet/Stack & Queue/Basic Learning/Implement stack using array.cpp
@@ -1,8 +1,7 @@
 // https://www.geeksforgeeks.org/problems/implement-stack-using-array/1
 
 
-//Brute: To push the element increase top and then arr[top] = x
-//To pop check if top is -1 then return -1, else return arr[top] and decrease top
+//Brute
 //Time: O(n)
 //Space: O(n)
 
@@ -23,8 +22,6 @@ void MyStack ::push(int x) {
     top++;
     arr[top] = x;
 }
-
-// Function to remove an item from top of the stack.
 int MyStack ::pop() {
     if(top == -1) return -1;
     return arr[top--];
