a400453b84c2f3e5ce9daa61f0772139b5fec59d
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-24 17:38:52 +0530

    Create 116A - Tram.cpp

---
+++ CP/Codeforces/116A - Tram.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/116/A
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    vector<vector<int>> mat(n, vector<int>(2, 0));
+    for(int i=0;i<n;i++){
+        cin >> mat[i][0] >> mat[i][1];
+    }
+
+    int curr = mat[0][1], d = curr;
+    for(int i=1;i<n;i++){
+        curr -= mat[i][0];
+        curr += mat[i][1];
+        d = max(d, curr);
+    }
+
+    cout << d << endl;
+    return 0;
+}
