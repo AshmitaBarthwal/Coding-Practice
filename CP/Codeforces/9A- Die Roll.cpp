6aeedeec6357afcbc5902e63e4ec35ba87013247
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-31 14:10:15 +0530

    Create 9A- Die Roll.cpp

---
+++ CP/Codeforces/9A- Die Roll.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/problemset/problem/9/A
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int y, w;
+    cin >> y >> w;
+
+    string probs[7] = {"", "1/1", "5/6", "2/3", "1/2", "1/3", "1/6"};
+    int num = max(y, w);
+    cout << probs[num] << endl;
+    return 0;
+}
