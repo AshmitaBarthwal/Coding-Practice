215798e5bdebf6d133e330031f66cdf5aa073a1e
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-31 17:44:12 +0530

    Create 746A - Compote.cpp

---
+++ CP/Codeforces/746A - Compote.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/problemset/problem/746/A
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int sum = 0;
+    bool flag = true;
+    while(flag){
+        if(a * 2 <= b && ((a * 2) * 2) <= c){
+            sum = a + a * 2 + (a * 2) * 2;
+            flag = false;
+        }else a--;
+    }
+
+    cout << sum << endl;
+    return 0;
+}
