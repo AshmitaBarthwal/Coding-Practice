e2e4c6351fef88b17691418ec0a4601c10815c0c
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-31 16:13:11 +0530

    Create 80A- Panoramix's Prediction.cpp

---
+++ CP/Codeforces/80A- Panoramix's Prediction.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/80/A
+
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    int next = n + 1;
+    while (!isPrime(next)) next++;
+    
+    if (next == m) cout << "YES" << endl;
+    else cout << "NO" << endl;
+    
+    return 0;
+}
