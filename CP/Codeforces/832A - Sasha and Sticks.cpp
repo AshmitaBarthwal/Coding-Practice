65cbcd36d987282e70142db05f45220b73f195a6
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-01 01:10:02 +0530

    Create 832A - Sasha and Sticks.cpp

---
+++ CP/Codeforces/832A - Sasha and Sticks.cpp
@@ -0,0 +1,20 @@
+// https://codeforces.com/problemset/problem/832/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n, k;
+    cin >> n >> k;
+
+    long long moves = n / k;
+
+    if (moves % 2 == 1) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
