15928ade69185d402883f09c70fcc969aae2fe91
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-08-31 17:03:39 +0530

    Create 749A - Bachgold Problem.cpp

---
+++ CP/Codeforces/749A - Bachgold Problem.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/749/A
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    if(n % 2 == 1) {
+        n /= 2;
+        n--;
+        cout << n + 1 << endl << 3 << ' ';
+    } else {
+        n /= 2;
+        cout << n << endl;
+    }
+    
+    for(int i = 0; i < n; i++) cout << 2 << ' ';
+    cout << endl;
+}
