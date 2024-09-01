8955780c41ae861a57cf441ebb19689c63c8cede
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-01 15:29:51 +0530

    Create 672A - Summer Camp.cpp

---
+++ CP/Codeforces/672A - Summer Camp.cpp
@@ -0,0 +1,23 @@
+// https://codeforces.com/problemset/problem/672/A
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string str = "";
+    int curr = 1;
+
+    while (str.length() < n) {
+        str += to_string(curr);
+        curr++;
+    }
+
+    cout << str[n - 1] << endl;
+
+    return 0;
+}
