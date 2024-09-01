a72ce87c7ab63b0d79b657b43ef6954d686be704
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-01 14:10:10 +0530

    Create 703A - Mishka and Game.cpp

---
+++ CP/Codeforces/703A - Mishka and Game.cpp
@@ -0,0 +1,31 @@
+// https://codeforces.com/problemset/problem/703/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int m, c, ms = 0, cs = 0;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> m >> c;
+        if(m > c) {
+            ms++;
+        } else if(c > m) {
+            cs++;
+        }
+        // No need to handle the case when m == c, as no points are awarded
+    }
+    
+    if(ms > cs) {
+        cout << "Mishka" << endl;
+    } else if(cs > ms) {
+        cout << "Chris" << endl;
+    } else {
+        cout << "Friendship is magic!^^" << endl;
+    }
+    
+    return 0;
+}
