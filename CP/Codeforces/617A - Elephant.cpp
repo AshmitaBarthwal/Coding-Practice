d09e62d6e7346bbf31da759460c45eba56c3c8b3
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 11:42:35 +0530

    Create 617A - Elephant.cpp

---
+++ CP/Codeforces/617A - Elephant.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/617/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int x;
+    cin >> x;
+    
+    int steps = x / 5;
+    if (x % 5 != 0) {
+        steps += 1;
+    }
+    
+    cout << steps << endl;
+    return 0;
+}
