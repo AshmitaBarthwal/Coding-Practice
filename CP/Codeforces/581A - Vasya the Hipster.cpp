b7b8e8fb1eda849e126606f786901ed7f84c8891
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 23:34:22 +0530

    Create 581A - Vasya the Hipster.cpp

---
+++ CP/Codeforces/581A - Vasya the Hipster.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/581/A
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a >= b){
+        a -= b;
+        a /= 2;
+        cout << b << ' ' << a << endl;
+    }else{
+        b -= a;
+        b /= 2;
+        cout << a << ' ' << b << endl;
+    }
+    return 0;
+}
