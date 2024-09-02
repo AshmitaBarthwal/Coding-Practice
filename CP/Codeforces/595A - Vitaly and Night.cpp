6247df7df7cc0502513afc0d77b7f9b9b2852b67
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 23:03:59 +0530

    Create 595A - Vitaly and Night.cpp

---
+++ CP/Codeforces/595A - Vitaly and Night.cpp
@@ -0,0 +1,32 @@
+// https://codeforces.com/problemset/problem/595/A
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long long i,j,n,x,m,sum,a[200][200];
+    while(cin>>n>>m)
+    {
+        long int ans=0;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=2*m;j++) cin>>a[i][j];
+        }
+
+        for(i=1;i<=n;i++)
+        {
+            for(j=2;j<=2*m;j+=2)
+            {
+
+                    if(a[i][j]!=0 ||a[i][j-1]!=0)
+                    ans++;
+            }
+        }
+        cout<<ans<<endl;
+    }
+    return  0;
+}
