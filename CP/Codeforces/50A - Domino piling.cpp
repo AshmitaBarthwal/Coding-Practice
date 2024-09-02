fab2d870e3208a316060662bbfc5a98d0ff14a0f
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 23:56:02 +0530

    Create 50A - Domino piling.cpp

---
+++ CP/Codeforces/50A - Domino piling.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/50/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int M, N;
+    cin >> M >> N;
+    int max_dominoes = (M * N) / 2;
+    cout << max_dominoes << endl;
+    return 0;
+}
