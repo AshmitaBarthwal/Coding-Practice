3b7f2e393d3940e52c6ee54a1f8518e3cf31ce9f
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 11:13:11 +0530

    Create 630A - Again Twenty Five!.cpp

---
+++ CP/Codeforces/630A - Again Twenty Five!.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/630/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    if (n >= 2) cout << "25" << endl;    
+    return 0;
+}
