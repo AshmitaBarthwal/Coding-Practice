c2b9001f51112399f68e91dcb84f3a6a5b99fc32
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-02 22:49:41 +0530

    Create 599A - Patrick and Shopping.cpp

---
+++ CP/Codeforces/599A - Patrick and Shopping.cpp
@@ -0,0 +1,22 @@
+// https://codeforces.com/problemset/problem/599/A
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define ll long long
+
+int main()
+{
+    ll d1,d2,d3,a1,a2,a3,a4,ans;
+    while(cin>>d1>>d2>>d3)
+    {
+      a1=d1+d2+d3;
+      a2=2*d1+2*d2;
+      a3=2*d1+2*d3;
+      a4=2*d2+2*d3;
+      ans=min(a1,min(a2,min(a3,a4)));
+      cout<<ans<<endl;
+    }
+    return 0;
+}
