7055fe8db2cd6363c98f5eed0486d77dc3d0df8c
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-03 10:19:07 +0530

    Create 133A- HQ9+.cpp

---
+++ CP/Codeforces/133A- HQ9+.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/133/A
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    for(char ch : str){
+        if(ch == 'H' || ch == 'Q' || ch == '9'){
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+    return 0;
+}
