8e0a0c36a874941abb6f95684574e53ce48f72d4
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-06 01:45:10 +0530

    Create 118A - String Task.cpp

---
+++ CP/Codeforces/118A - String Task.cpp
@@ -0,0 +1,26 @@
+// https://codeforces.com/problemset/problem/118/A
+
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    string str;
+    cin >> str;
+    string result = "";
+
+    string vowel = "aeiyouAEIOYU";
+    for(char ch : str){
+        if(vowel.find(ch) == string::npos){
+            ch = tolower(ch);
+            result += '.';
+            result += ch;
+        }
+    }
+
+    cout << result << endl;
+    return 0;
+}
