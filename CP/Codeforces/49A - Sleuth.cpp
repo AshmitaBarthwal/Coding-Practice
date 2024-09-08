3793c7c3e9cc7da682df82f8961f17a9ceb26133
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-08 11:24:26 +0530

    Create 49A - Sleuth.cpp

---
+++ CP/Codeforces/49A - Sleuth.cpp
@@ -0,0 +1,27 @@
+//https://codeforces.com/problemset/problem/49/A
+
+
+#include <iostream>
+#include <cctype>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string question;
+    getline(cin, question); 
+    string vowels = "AEIOUYaeiouy";
+    for (int i = question.length() - 2; i >= 0; --i) 
+    {  
+        if (isalpha(question[i])) {
+            if (vowels.find(question[i]) != string::npos) 
+            {
+                cout << "YES" << endl;
+            } else {
+                cout << "NO" << endl;
+            }
+            return 0;  
+        }
+    }
+    return 0;
+}
