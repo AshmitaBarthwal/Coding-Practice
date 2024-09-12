35ae919d2ce8e31d743def0a26e1cfd36e34ccff
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-12 09:12:42 +0530

    Update 44A - Indian Summer.cpp

--- CP/Codeforces/44A - Indian Summer.cpp
+++ CP/Codeforces/44A - Indian Summer.cpp
@@ -8,12 +8,11 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
     cin >> n;
-
     set<pair<string, string>> uniqueLeaves;
-
     for (int i = 0; i < n; ++i) {
         string species, color;
         cin >> species >> color;
