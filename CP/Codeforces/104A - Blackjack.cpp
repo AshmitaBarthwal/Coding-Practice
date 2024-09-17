922ff7c980d81cb056acb26da856b76dd61f7918
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-17 14:52:03 +0530

    Update 104A - Blackjack.cpp

--- CP/Codeforces/104A - Blackjack.cpp
+++ CP/Codeforces/104A - Blackjack.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int n;
@@ -10,5 +11,6 @@ int main()
     if (n <= 10 || n > 21) cout << 0 << endl;
     else if (n == 20) cout << 15 << endl;
     else cout << 4 << endl;
+    
     return 0;
 }
