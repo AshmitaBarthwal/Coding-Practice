a92be9a63aac4c525d863bad6929636ab271eed4
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-22 12:31:31 +0530

    Create 476A - Dreamoon and Stairs.cpp

---
+++ CP/Codeforces/476A - Dreamoon and Stairs.cpp
@@ -0,0 +1,21 @@
+// https://codeforces.com/problemset/problem/476/A
+
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    int lb = (n+1)/2;
+    int result = (lb + m - 1)/m * m;
+
+    if(result > n) result = -1;
+    cout << result << endl;
+
+    return 0;
+}
