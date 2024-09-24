69ac35ed3ca0c606ab73eb0aa1f30484d015ea9a
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-09-24 12:10:24 +0530

    Update 758A - Holiday Of Equality.cpp

--- CP/Codeforces/758A - Holiday Of Equality.cpp
+++ CP/Codeforces/758A - Holiday Of Equality.cpp
@@ -9,19 +9,19 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
     vector<int> welfare(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cin >> welfare[i];
     }
 
     int max_welfare = *max_element(welfare.begin(), welfare.end());
     int total_cost = 0;
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         total_cost += (max_welfare - welfare[i]);
     }
-
     cout << total_cost << endl;
     return 0;
 }
