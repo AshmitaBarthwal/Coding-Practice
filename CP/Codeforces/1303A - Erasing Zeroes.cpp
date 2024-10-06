ebe166f67e61f13af9004838d7b8b952f1b51cd7
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-06 14:53:17 +0530

    Update 1303A - Erasing Zeroes.cpp

--- CP/Codeforces/1303A - Erasing Zeroes.cpp
+++ CP/Codeforces/1303A - Erasing Zeroes.cpp
@@ -10,34 +10,41 @@ using namespace std;
 int main(){
     int t;
     cin >> t;
-    while(t--){
+    while(t--)
+    {
         string str;
         cin >> str;
 
         int n = str.length();
         int left = -1, right = n;
 
-        for(int i=0;i<n;i++){
-            if(str[i] == '1'){
+        for(int i=0;i<n;i++)
+        {
+            if(str[i] == '1')
+            {
                 left = i;
                 break;
             }
         }
 
-        for(int i=n-1;i>=0;i--){
-            if(str[i] == '1'){
+        for(int i=n-1;i>=0;i--)
+        {
+            if(str[i] == '1')
+            {
                 right = i;
                 break;
             }
         }
 
-        if(left == -1 && right == n){
+        if(left == -1 && right == n)
+        {
             cout << 0 << endl;
             continue;
         }
 
         int count = 0;
-        for(int i=left;i<=right;i++){
+        for(int i=left;i<=right;i++)
+        {
             if(str[i] == '0') count++;
         }
 
