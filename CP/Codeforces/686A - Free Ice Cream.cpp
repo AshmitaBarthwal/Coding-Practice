b1e308632d91cd9bdc9e523092180c2b10539fbb
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-09 21:48:05 +0530

    Update 686A - Free Ice Cream.cpp

--- CP/Codeforces/686A - Free Ice Cream.cpp
+++ CP/Codeforces/686A - Free Ice Cream.cpp
@@ -4,20 +4,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
     long long x;
     cin >> n >> x;
     
     int count = 0;
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         char operation;
         long long di;
         cin >> operation >> di;
         
         if (operation == '+') x += di;
-        else if (operation == '-') {
+        else if (operation == '-') 
+        {
             if (x >= di) x -= di;
             else count++;
         }
