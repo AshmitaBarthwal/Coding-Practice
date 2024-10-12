1e2a4452644f35a24f6083e8a730d3c91faece80
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-12 12:41:50 +0530

    Update 1391B - Fix You.cpp

--- CP/Codeforces/1391B - Fix You.cpp
+++ CP/Codeforces/1391B - Fix You.cpp
@@ -11,13 +11,16 @@ using namespace std;
 int main(){
     int t;
     cin >> t;
-    while(t--){
+    while(t--)
+    {
         int n, m;
         cin >> n >> m;
 
         int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
                 char dir;
                 cin >> dir;
                 if(j == m-1 && dir == 'R') count++;
