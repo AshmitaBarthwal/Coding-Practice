c678b87dbcad99cb731b451aed0c8c4131747cd3
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-19 13:31:24 +0530

    Update 52A 123-sequence.cpp

--- CP/Codeforces/52A 123-sequence.cpp
+++ CP/Codeforces/52A 123-sequence.cpp
@@ -4,24 +4,28 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int main() {
+int main() 
+{
     int n;
     cin >> n;
     vector<int> arr(n);
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cin >> arr[i];
     }
     
     map<int, int> mp;
     
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         mp[arr[i]]++;
     }
     
     int maxi = 0;
     
-    for (auto it : mp) {
+    for (auto it : mp) 
+    {
         maxi = max(maxi, it.second);
     }
     
