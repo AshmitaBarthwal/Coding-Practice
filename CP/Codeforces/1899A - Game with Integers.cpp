32b762aee16dac904c0fdaefb3e0ed5fade5b837
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-10-31 16:52:43 +0530

    Create 1899A - Game with Integers.cpp

---
+++ CP/Codeforces/1899A - Game with Integers.cpp
@@ -0,0 +1,19 @@
+// https://codeforces.com/problemset/problem/1899/A
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        
+        if(n % 3) cout << "First" << endl;
+        else cout << "Second" << endl;
+    }
+
+    return 0;
+}
