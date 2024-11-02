80f5e9f98414483d9abea612d727c2704f591179
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-03 01:01:24 +0530

    Update 58A - Chat room.cpp

--- CP/Codeforces/58A - Chat room.cpp
+++ CP/Codeforces/58A - Chat room.cpp
@@ -5,17 +5,21 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     string s;
     cin >> s;
     string target = "hello";
     int j = 0;
     
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == target[j]) {
+    for (int i = 0; i < s.size(); i++) 
+    {
+        if (s[i] == target[j]) 
+        {
             j++;
         }
-        if (j == target.size()) {
+        if (j == target.size()) 
+        {
             cout << "YES" << endl;
             return 0;
         }
