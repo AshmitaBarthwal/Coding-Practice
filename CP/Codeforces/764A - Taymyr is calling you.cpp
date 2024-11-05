8e83486638a0eccb8a77da80f47539a992ae8a2a
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-05 11:40:40 +0530

    Update 764A - Taymyr is calling you.cpp

--- CP/Codeforces/764A - Taymyr is calling you.cpp
+++ CP/Codeforces/764A - Taymyr is calling you.cpp
@@ -11,13 +11,11 @@ int main()
 {
     int n, m, z;
     cin >> n >> m >> z;
-
     int count = 0;
     for(int i=1;i<=z;i++)
     {
         if(i % n == 0 && i % m == 0) count++;
     }
-
     cout << count << endl;
     return 0;
 }
