d6c2ceccd7a56c9cdcafc5b43e283cbf60f77ea4
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-08 11:22:30 +0530

    Create 1030A - In Search of an Easy Problem.cpp

---
+++ CP/Codeforces/1030A - In Search of an Easy Problem.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/problemset/problem/1030/A
+
+
+#include <iostream>
+#include <vector>;
+#include <algorithm>;
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++)
+    {
+        int num;
+        cin >> num;
+        if(num == 1)
+        {
+            cout << "HARD" << endl;
+            return 0;
+        }
+    }
+    
+    cout << "EASY" << endl;
+    return 0;
+}
