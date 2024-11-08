fdc396f98c977b03446ad526f781027b6d554875
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-08 11:19:52 +0530

    Create 1081A - Definite Game.cpp

---
+++ CP/Codeforces/1081A - Definite Game.cpp
@@ -0,0 +1,12 @@
+// https://codeforces.com/problemset/problem/1081/A
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    if(n == 2) cout<<"2"<<endl;
+    else cout<<"1"<<endl;
+}
