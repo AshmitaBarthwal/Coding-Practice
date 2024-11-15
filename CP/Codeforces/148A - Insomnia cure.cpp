302304d062865d515e0c5ea3b9c6f3d0d1bf3ab7
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-15 12:46:10 +0530

    Update 148A - Insomnia cure.cpp

--- CP/Codeforces/148A - Insomnia cure.cpp
+++ CP/Codeforces/148A - Insomnia cure.cpp
@@ -4,14 +4,17 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int k, l, m, n, d;
     cin >> k >> l >> m >> n >> d;
 
     int count = 0;
 
-    for(int i = 1; i <= d; i++) {
-        if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) {
+    for(int i = 1; i <= d; i++) 
+    {
+        if(i % k == 0 || i % l == 0 || i % m == 0 || i % n == 0) 
+        {
             count++;
         }
     }
