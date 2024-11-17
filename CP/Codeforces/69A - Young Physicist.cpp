6651cf9a2f91cced89051aaf4d0614819b7f73f7
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2024-11-17 15:10:00 +0530

    Update 69A - Young Physicist.cpp

--- CP/Codeforces/69A - Young Physicist.cpp
+++ CP/Codeforces/69A - Young Physicist.cpp
@@ -4,24 +4,26 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n;
     cin >> n;
-    
     int sumX = 0, sumY = 0, sumZ = 0;
-    
-    for (int i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) 
+    {
         int x, y, z;
         cin >> x >> y >> z;
         sumX += x;
         sumY += y;
         sumZ += z;
     }
-    
     // Check if all sums are zero
-    if (sumX == 0 && sumY == 0 && sumZ == 0) {
+    if (sumX == 0 && sumY == 0 && sumZ == 0) 
+    {
         cout << "YES" << endl;
-    } else {
+    } 
+    else 
+    {
         cout << "NO" << endl;
     }
     
