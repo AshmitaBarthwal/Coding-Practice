033c9b5f23ab9a1f4f1962794ff9151d2851b5ee
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-03 20:36:40 +0530

    Create 	721A - One-dimensional Japanese Crossword.cpp

---
+++ CP/Codeforces/721A - One-dimensional Japanese Crossword.cpp
@@ -0,0 +1,32 @@
+//https://codeforces.com/problemset/problem/721/A
+
+
+#include <bits\stdc++.h>
+using namespace std;
+int main()
+{
+	int n, x = 0, c = 0;
+	scanf("%d", &n);
+	string s;
+	cin >> s;
+	bool f = 0;
+	int a[n];
+	for (int i = 0; i <= n; i++)
+	{
+		if (s[i] == 'B')
+		{
+			f = 1;
+			x++;
+		}
+		else if (f)
+		{
+			a[c] = x;
+			x = 0;
+			f = 0;
+			c++;
+		}
+	}
+	cout << c << endl;
+	for (int i = 0; i < c; i++)
+		cout << a[i] << " ";
+}
