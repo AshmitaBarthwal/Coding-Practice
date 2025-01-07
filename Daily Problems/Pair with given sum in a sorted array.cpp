c5c60f9b01e4e69420f1a7710fb1394697a5f7c1
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-07 16:40:39 +0530

    Create Pair with given sum in a sorted array.cpp

---
+++ Daily Problems/Pair with given sum in a sorted array.cpp
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/pair-with-given-sum-in-a-sorted-array4940/1
+
+
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) 
+    {
+        unordered_map<int,int>res;
+        int count=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            int temp=target-arr[i];
+            
+            if(res.find(temp)!=res.end()){
+                count+=res[temp];
+            }
+            res[arr[i]]++;
+        }
+        return count;
+    }
+};
