21587f9e6fb423c97cb8dc810880a53efa32d138
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-08 13:19:53 +0530

    Update 347A - Difference Row.cpp

--- CP/Codeforces/347A - Difference Row.cpp
+++ CP/Codeforces/347A - Difference Row.cpp
@@ -1,46 +1,25 @@
 //https://codeforces.com/problemset/problem/347/A
 
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
+    long long n;
     cin >> n;
 
-    vector<int> a(n);
-    for (int i = 0; i < n; ++i) {
+    vector<long long> a(n);
+    for (long long i = 0; i < n; i++) 
+    {
         cin >> a[i];
     }
-
-    // Sort the array to arrange lexicographically smallest sequence
     sort(a.begin(), a.end());
-
-    vector<int> result;
-
-    // Use two pointers to create the required sequence
-    int left = 0, right = n - 1;
-    while (left <= right) {
-        if (left == right) {
-            result.push_back(a[right]);
-        } else {
-            result.push_back(a[right]);
-            result.push_back(a[left]);
-        }
-        --right;
-        ++left;
+    swap(a[0], a[n - 1]);
+    for (long long i = 0; i < n; i++) {
+        cout << a[i] << " ";
     }
-
-    // Reverse the result to get lexicographically smallest sequence
-    reverse(result.begin(), result.end());
-
-    for (int x : result) {
-        cout << x << " ";
-    }
-    cout << endl;
 
     return 0;
 }
+
 
