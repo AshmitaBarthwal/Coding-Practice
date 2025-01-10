1cf0ac8d2d4dc161683f8f59f1420a5a5c7728c0
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-10 20:40:09 +0530

    Create 916. Word Subsets.cpp

---
+++ Daily Problems/916. Word Subsets.cpp
@@ -0,0 +1,39 @@
+//https://leetcode.com/problems/word-subsets/description/
+
+
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) 
+    {
+        vector<int> maxFreq(26, 0); 
+        for (const string& word : words2) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                maxFreq[i] = max(maxFreq[i], freq[i]);
+            }
+        }
+        
+        vector<string> result;
+        for (const string& word : words1) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+            bool isUniversal = true;
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] < maxFreq[i]) {
+                    isUniversal = false;
+                    break;
+                }
+            }
+            if (isUniversal) {
+                result.push_back(word);
+            }
+        }
+        
+        return result;
+    }
+};
