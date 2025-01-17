352007c2f00571677d6048d75678455b4857f48c
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-17 23:46:24 +0530

    Create Product array puzzle.cpp

---
+++ Daily Problems/Product array puzzle.cpp
@@ -0,0 +1,45 @@
+//https://www.geeksforgeeks.org/problems/product-array-puzzle4525/1
+
+
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) 
+    {
+         int n = arr.size();
+        vector<int> res(n);
+
+        int product = 1;
+        int zeroCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                zeroCount++;
+            } else {
+                product *= arr[i];
+            }
+        }
+
+        if (zeroCount > 1) {
+            return vector<int>(n, 0); // All zeros
+        }
+
+
+        if (zeroCount == 1) {
+            for (int i = 0; i < n; i++) {
+                if (arr[i] == 0) {
+                    res[i] = product;
+                } else {
+                    res[i] = 0;
+                }
+            }
+            return res;
+        }
+
+        for (int i = 0; i < n; i++) {
+            res[i] = product / arr[i];
+        }
+
+        return res;
+    }
+};
