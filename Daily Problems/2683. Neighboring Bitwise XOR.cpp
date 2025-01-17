a4577cbfb7947eab55251959d61abf090d9c7fda
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-01-17 23:51:51 +0530

    Create 2683. Neighboring Bitwise XOR.cpp

---
+++ Daily Problems/2683. Neighboring Bitwise XOR.cpp
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/neighboring-bitwise-xor/description/
+
+
+
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) 
+    {
+        int n = derived.size();
+        vector<int> original(n);
+        original[0] = 0;
+        for(int i = 0; i < n-1; i++) {
+            original[i+1] = original[i] ^ derived[i];
+        }
+
+        if((original[n-1] ^ original[0]) == derived[n-1]) {
+            return true;
+        }
+        original[0] = 1;
+        for(int i = 0; i < n-1; i++) {
+            original[i+1] = original[i] ^ derived[i];
+        }
+
+        if((original[n-1] ^ original[0]) == derived[n-1]) {
+            return true;
+        }
+
+        return false;
+    }
+};
