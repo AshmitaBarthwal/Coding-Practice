3c47688ef33c1597acb8285c30bb6b7db236da3b
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-02-09 21:41:07 +0530

    Create 2364. Count Number of Bad Pairs.cpp

---
+++ Daily Problems/2364. Count Number of Bad Pairs.cpp
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/count-number-of-bad-pairs/description/
+
+
+
+class Solution {
+public:
+    long long countBadPairs(vector<int>& nums) 
+    {
+        int n = nums.size();
+        long long totalPairs = (long long)n * (n - 1) / 2;
+        unordered_map<int, long long> diffCount;
+        for (int i = 0; i < n; i++) {
+            int diff = nums[i] - i;
+            totalPairs -= diffCount[diff];
+            diffCount[diff]++;
+        }
+        return totalPairs;
+    }
+};
