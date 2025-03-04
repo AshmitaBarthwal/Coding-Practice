5b3bbeaa9664575cbe5168ec832f1b148d90a349
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-03-04 23:27:59 +0530

    Create Longest Increasing Subsequence.cpp

---
+++ Daily Problems/Longest Increasing Subsequence.cpp
@@ -0,0 +1,24 @@
+//https://www.geeksforgeeks.org/problems/longest-increasing-subsequence-1587115620/1
+
+
+
+class Solution {
+  public:
+    int lis(vector<int>& arr) 
+    {
+        int n=arr.size();
+        vector<int>maxi(n, 1);
+        int ans=1;
+        
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<i; j++)
+            {
+                if(arr[j]<arr[i]) maxi[i]=max(maxi[i], maxi[j]+1);
+            }
+            ans=max(ans, maxi[i]);
+        }
+        
+        return ans;
+    }
+};
