d38af90101255de0aa879b102a4e1cc9825b73b3
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-04-10 17:28:34 +0530

    Create 2999. Count the Number of Powerful Integers.cpp

---
+++ Daily Problems/2999. Count the Number of Powerful Integers.cpp
@@ -0,0 +1,44 @@
+//https://leetcode.com/problems/count-the-number-of-powerful-integers/?envType=daily-question&envId=2025-04-10
+
+
+
+class Solution {
+public:
+    long long solve(string& str, string& inputSuffix, int limit) 
+    {
+        if(str.length() < inputSuffix.length()) 
+        {
+            return 0;
+        }
+
+        long long count = 0;
+        string trailString = str.substr(str.length() - inputSuffix.length());
+        int remainL = str.length() - inputSuffix.length();
+
+        for(int i = 0; i < remainL; i++) {
+            int digit = str[i] - '0';
+
+            if(digit <= limit) {
+                count += digit * pow(limit+1, remainL-i-1);
+            } else {
+                count += pow(limit+1, remainL-i); //5^pos
+                return count;
+            }
+        }
+
+        if(trailString >= inputSuffix) {
+            count += 1;
+        }
+
+
+        return count;
+
+    }
+    long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) 
+    {
+        string startStr = to_string(start-1);
+        string finishStr = to_string(finish);
+
+        return solve(finishStr, s, limit) - solve(startStr, s, limit);
+    }
+};
