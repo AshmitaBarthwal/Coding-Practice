d6727d1fa1f6da4cf17545244288a6a820a7de4e
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-04-27 23:23:57 +0530

    Update Multiply two strings.cpp

--- Daily Problems/Multiply two strings.cpp
+++ Daily Problems/Multiply two strings.cpp
@@ -7,34 +7,34 @@ class Solution {
     /*You are required to complete below function */
     string multiplyStrings(string& s1, string& s2) 
     {
+         bool neg = false;
+        if (s1[0] == '-') 
+        {
+            neg = !neg;
+            s1 = s1.substr(1);
+        }
+        if (s2[0] == '-') {
+            neg = !neg;
+            s2 = s2.substr(1);
+        }
+        while (s1.size() > 1 && s1[0] == '0') s1.erase(s1.begin());
+        while (s2.size() > 1 && s2[0] == '0') s2.erase(s2.begin());
         if (s1 == "0" || s2 == "0") return "0";
-        
-        int m = s1.size(), n = s2.size();
-        vector<int> result(m + n, 0);
-        
-        // Multiply each digit from s1 with each digit from s2
-        for (int i = m - 1; i >= 0; --i) 
-        {
-            for (int j = n - 1; j >= 0; --j) 
-            {
+        int n = s1.size(), m = s2.size();
+        vector<int> res(n + m, 0);
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
                 int mul = (s1[i] - '0') * (s2[j] - '0');
-                int sum = mul + result[i + j + 1];
-                
-                result[i + j + 1] = sum % 10; // store single digit
-                result[i + j] += sum / 10;   // carry to next higher place
+                int sum = mul + res[i + j + 1];
+                res[i + j] += sum / 10;
+                res[i + j + 1] = sum % 10;
             }
         }
-        
-        // Convert result vector to string
-        string res = "";
-        for (int i = 0; i < result.size(); ++i) 
-        {
-            if (!(res.empty() && result[i] == 0)) 
-            {
-                res.push_back(result[i] + '0');
-            }
-        }
-        
-        return res;
+        string ans;
+        int i = 0;
+        while (i < res.size() && res[i] == 0) i++;
+        for (; i < res.size(); i++) ans.push_back(res[i] + '0');
+        if (neg) ans = "-" + ans;
+        return ans;
     }
 };
