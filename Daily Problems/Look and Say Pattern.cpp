9749149191b7564536949c36fe187bd566d0bd44
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-05-14 22:35:26 +0530

    Create Look and Say Pattern.cpp

---
+++ Daily Problems/Look and Say Pattern.cpp
@@ -0,0 +1,36 @@
+//https://www.geeksforgeeks.org/problems/decode-the-pattern1138/1
+
+
+
+
+class Solution 
+{
+  public:
+    string countAndSay(int n) 
+    {
+        string res="1", temp="";
+        
+        for(int i=1; i<n; i++)
+        {
+            int count=1;
+            
+            for(int j=0; j<res.size(); j++)
+            {
+                while(res[j]==res[j+1])
+                {
+                    
+                    count++;
+                    j++;
+                }
+                temp = temp + to_string(count) + res[j];
+                
+                count=1;
+            }
+            res=temp;
+            
+            temp="";
+        }
+        
+        return res;
+    }
+};
