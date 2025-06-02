c151b1f0022bfa263a973a821d7ea6e45b17f192
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-06-02 15:29:55 +0530

    Update Unique Paths in a Grid.cpp

--- Daily Problems/Unique Paths in a Grid.cpp
+++ Daily Problems/Unique Paths in a Grid.cpp
@@ -7,29 +7,27 @@ class Solution {
   public:
     int uniquePaths(vector<vector<int>> &grid) 
     {
-        int r=grid.length;
-        int c=grid[0].length;
-        int[][] dp=new int[r][c];
-        for(int i=0;i<c;i++)
-        {
-            if(grid[0][i]==0) dp[0][i]=1;
-            else break;
-        }
-        for(int i=0;i<r;i++)
-        {
-            if(grid[i][0]==0) dp[i][0]=1;
-            else break;
-        }
-        for(int i=1;i<r;i++)
-        {
-            for(int j=1;j<c;j++)
-            {
-                if(grid[i][j]==0)
-                {
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        int n, m;
+        n=grid.size();
+        m=grid[0].size();
+        
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return 0;
+        
+        vector<int>prev(m, 0);
+        
+        for(int i=0; i<n; i++){
+            vector<int>curr(m, 0);
+            for(int j=0; j<m; j++){
+                if(grid[i][j]==1) continue;
+                if (i == 0 && j == 0) {
+                    curr[j] = 1;
+                } else {
+                    if (i > 0) curr[j] += prev[j];
+                    if (j > 0) curr[j] += curr[j - 1];
                 }
             }
+            prev=curr;
         }
-        return dp[r-1][c-1];
+        return prev[m-1];
     }
 };
