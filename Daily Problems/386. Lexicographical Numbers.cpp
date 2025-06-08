2ff0a58c0bcf3ba2a7a6660bced98eec119b3caf
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-06-08 14:23:52 +0530

    Create 386. Lexicographical Numbers.cpp

---
+++ Daily Problems/386. Lexicographical Numbers.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/lexicographical-numbers/description/?envType=daily-question&envId=2025-06-08
+
+
+
+class Solution {
+public:
+    void solve(int curr, int n, vector<int>& result) {
+        if(curr > n)
+            return;
+        
+        result.push_back(curr);
+
+        for(int nextDigit = 0; nextDigit <= 9; nextDigit++) {
+            int nextnum = curr*10 + nextDigit;
+            
+            if(nextnum > n)
+                return;
+            
+            solve(nextnum, n, result);
+        }
+    }
+
+    vector<int> lexicalOrder(int n) {
+        vector<int> result;
+        
+        for(int num = 1; num <= 9; num++)
+            solve(num, n, result);
+        
+        return result; 
+    }
+};
