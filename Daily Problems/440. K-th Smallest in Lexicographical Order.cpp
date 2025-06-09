89c1c05e427c54a88527a342e808211a0e2bed14
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-06-09 22:32:11 +0530

    Create 440. K-th Smallest in Lexicographical Order.cpp

---
+++ Daily Problems/440. K-th Smallest in Lexicographical Order.cpp
@@ -0,0 +1,35 @@
+/https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/?envType=daily-question&envId=2025-06-09
+
+
+
+
+class Solution {
+public:
+    int findKthNumber(int n, int k) {
+    long ans = 1;
+
+    for (int i = 1; i < k;) {
+      const long gap = getGap(ans, ans + 1, n);
+      if (i + gap <= k) {
+        i += gap;
+        ++ans;
+      } else {
+        ++i;
+        ans *= 10;
+      }
+    }
+
+    return ans;
+  }
+
+ private:
+  long getGap(long a, long b, long n) {
+    long gap = 0;
+    while (a <= n) {
+      gap += min(n + 1, b) - a;
+      a *= 10;
+      b *= 10;
+    }
+    return gap;
+  }
+};
