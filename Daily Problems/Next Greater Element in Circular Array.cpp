288ecae42f3bbb565caea88073bc7577f4b26862
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-07-07 23:44:42 +0530

    Create Next Greater Element in Circular Array.cpp

---
+++ Daily Problems/Next Greater Element in Circular Array.cpp
@@ -0,0 +1,22 @@
+//https://www.geeksforgeeks.org/problems/next-greater-element/1
+
+
+
+class Solution {
+  public:
+    vector<int> nextLargerElement(vector<int> &arr) 
+    {
+         int  n = arr.size();
+        stack<int>st;
+        vector<int>ans(n,-1);
+        for(int i=0 ; i<n*2 ; i++){
+            while(!st.empty() && arr[st.top()]<arr[i%n]){
+                  ans[st.top()] = arr[i%n];
+              st.pop();
+          }
+          if(i<n)
+          st.push(i);
+      }
+      return ans;
+    }
+};
