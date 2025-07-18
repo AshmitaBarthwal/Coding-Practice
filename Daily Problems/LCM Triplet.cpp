29d5479765745d6ed886c903ab518062534cc788
Ashmita Barthwal <149078715+AshmitaBarthwal@users.noreply.github.com>
2025-07-19 00:00:18 +0530

    Create LCM Triplet.cpp

---
+++ Daily Problems/LCM Triplet.cpp
@@ -0,0 +1,16 @@
+//https://www.geeksforgeeks.org/problems/lcm-triplet1501/1
+
+
+
+
+class Solution {
+  public:
+    int lcmTriplets(int n) {
+         if(n<=2) return n;
+        if(n%2==0 && n%3==0)
+            return (n-2)*(n-1)*(n-3);
+        else if(n%2==0)
+            return n*(n-1)*(n-3);
+        return n*(n-1)*(n-2);
+    }
+};
